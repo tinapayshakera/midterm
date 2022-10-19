95b0e7887fd00df6040cbb8777f2f82e3a46a7d1
tinapayshakera <112525458+tinapayshakera@users.noreply.github.com>
2022-10-19 18:58:02 +0800

    Add files via upload

---
+++ ATM_MIDTERM_TINAPAY/ATM.java
@@ -0,0 +1,58 @@
+package atm.simulation.java;
+import java.io.*;
+public class ATMSimulationJava {
+    
+}
+public class GFG {
+
+    
+    public static void displayBalance(int balance) {
+        System.out.println("Current Balance : " + balance);
+        System.out.println();
+    }
+
+    
+    public static int amountWithdrawing(int balance,
+            int withdrawAmount) {
+        System.out.println("Withdrawn Operation:");
+        System.out.println("Withdrawing Amount : "
+                + withdrawAmount);
+        if (balance >= withdrawAmount) {
+            balance = balance - withdrawAmount;
+            System.out.println(
+                    "Please collect your money and collect the card");
+            displayBalance(balance);
+        } else {
+            System.out.println("Sorry! Insufficient Funds");
+            System.out.println();
+        }
+        return balance;
+    }
+
+    
+    public static int amountDepositing(int balance,
+            int depositAmount) {
+        System.out.println("Deposit Operation:");
+        System.out.println("Depositing Amount : "
+                + depositAmount);
+        balance = balance + depositAmount;
+        System.out.println(
+                "Your Money has been successfully deposited");
+        displayBalance(balance);
+        return balance;
+    }
+
+    public static void main(String args[]) {
+        int balance = 10000;
+        int withdrawAmount = 5000;
+        int depositAmount = 2000;
+
+        // calling display balance
+        displayBalance(balance);
+        // withdrawing amount
+        balance
+                = amountWithdrawing(balance, withdrawAmount);
+        // depositing amount
+        balance = amountDepositing(balance, depositAmount);
+    }
+}
---
+++ ATM_MIDTERM_TINAPAY/Account.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm.simulation.java;
+
+import java.util.Date;
+
+/**
+ *
+ * @author student.admin
+ */
+class Account {
+
+    Account(int i, double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setAnnualIntrestRate(double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setDateCreated(Date date) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object[] getDateCreated() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object[] getMonthlyInterestRate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object[] getMonthlyInterest() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object[] totalBalance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object[] withDraw(double withdramount) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object[] diposite(double dipositAmount) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
