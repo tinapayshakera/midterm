1c3326ae54bd3130ac011c4e28ee2d36e036ab5c
tinapayshakera <112525458+tinapayshakera@users.noreply.github.com>
2022-11-15 10:11:37 +0800

    Add files via upload

---
+++ Webdev_practice/ajax-jquery.js
@@ -0,0 +1,46 @@
+$(function(){
+    $(".input").focus(function() {
+        $("#search").addClass("move");
+      });
+      $(".input").focusout(function() {
+        $("#search").removeClass("move");
+        $(".input").val("");
+      });
+      
+      $(".fa-search").click(function() {
+        $(".input").toggleClass("active");
+        $("#search").toggleClass("active");
+      });
+      
+})
+$(document).ready(function() {
+  clockUpdate();
+  setInterval(clockUpdate, 1000);
+});
+
+function clockUpdate() {
+  var date = new Date();
+  $('.digital-clock').css({'color': 'white'});
+  function addZero(x) {
+      if (x < 10) {
+          return x = '0' + x;
+      } else {
+          return x;
+      }
+  }
+
+  function twelveHour(x) {
+      if (x > 12) {
+          return x = x - 12;
+      } else if (x == 0) {
+          return x = 12;
+      } else {
+          return x;
+      }
+  }
+
+  var h = addZero(twelveHour(date.getHours()));
+  var m = addZero(date.getMinutes());
+
+  $('.digital-clock').text(h + ':' + m )
+}     
